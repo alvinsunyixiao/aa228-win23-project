ecbd5f527e876048ac98154ea9c2869ec6ebdf43
Kaixin Chen <kaixinc@stanford.edu>
2023-03-15 14:15:06 -0700

    Three cars can avoid each other now

--- MPC.py
+++ MPC.py
@@ -9,7 +9,7 @@ import random
 
 
 class MPC:
-    def __init__(self, world, agent_idx, dest, gamma=0.95):
+    def __init__(self, world, agent_idx, dest, gamma=0.97):
         self.world = world
         self.agent_idx = agent_idx
         self.dest = dest
@@ -17,14 +17,14 @@ class MPC:
         self.collision_penalty = -10000
         self.arrival_reward = 1000
         self.control_penalty = 1
-        self.steering = [-0.2, 0, 0.2]
-        self.acc = [-0.3, 0, 0.3]
+        self.steering = [-0.3, 0.3]
+        self.acc = [-0.5, 0, 0.5]
         self.steering_std = np.std(np.array(self.steering)) * 3
-        self.acc_std = np.std(np.array(self.acc)) * 3
-        self.d = 60
-        self.m = 1
+        self.acc_std = np.std(np.array(self.acc)) * 2
+        self.d = 40
+        self.m = 2
         self.gamma = gamma
-        self.roll_out_policy_decay = 0.92
+        self.roll_out_policy_decay = 0.95
 
     def U_d(self, world):
         loc = world.dynamic_agents[self.agent_idx].center
@@ -49,7 +49,7 @@ class MPC:
             return self.roll_out_policy_decay * agent.angular_velocity, self.roll_out_policy_decay * agent.acceleration
 
         def rollout(self, h_world, d, steer=None, acc=None):
-            print(f"car {self.agent_idx}")
+            # print(f"car {self.agent_idx}")
             ret = 0
             for i in range(d):
                 if steer is None or acc is None:
@@ -63,8 +63,8 @@ class MPC:
                         curr_steer = np.random.normal(0.0, self.steering_std)
                         c.set_control(curr_steer, curr_acc)
                 h_world.tick()
-                #h_world.render()
-                #time.sleep(dt / 8)
+                # h_world.render()
+                # time.sleep(dt / 8)
                 steer, acc = None, None
             ret += self.U_d(h_world) * self.gamma ** self.m
 
@@ -88,22 +88,34 @@ class MPC:
 if __name__ == "__main__":
     dt = 0.1
     w = World(dt, width=120, height=120, ppm=6)
+
     renderer = copy.deepcopy(w)
-    c0 = Car(Point(50, 20),np.pi)
+
+    c0 = Car(Point(40, 20),np.pi)
     c0.velocity = Point(3.0, 0)
     w.add(c0)
+    controller0 = MPC(w, 0, np.array([10, 20]))
     renderer.add(c0)
+
     c1 = Car(Point(10, 20), 0, 'blue')
     c1.velocity = Point(3.0, 0)
     w.add(c1)
     renderer.add(c1)
-    controller0 = MPC(w, 0, np.array([10, 20]))
-    controller1 = MPC(w, 1, np.array([50, 20]))
-    for i in range(50):
+    controller1 = MPC(w, 1, np.array([40, 20]))
+
+    c2 = Car(Point(25, 5), np.pi/2, 'green')
+    c2.velocity = Point(3.0, 0)
+    w.add(c2)
+    renderer.add(c2)
+    controller2 = MPC(w, 2, np.array([25, 35]))
+
+    for i in range(200):
         s0, a0 = controller0.look_ahead_with_rollouts()
         s1, a1 = controller1.look_ahead_with_rollouts()
+        s2, a2 = controller2.look_ahead_with_rollouts()
         c0.set_control(s0, a0)
         c1.set_control(s1, a1)
+        c2.set_control(s2, a2)
         w.tick()
         renderer.tick()
         renderer.render()
